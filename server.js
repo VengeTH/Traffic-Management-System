8e2b7d88b72265e248c853214ce8ac17bf3972dc
Prinze Mikhail Sadsad <96782670+VengeTH@users.noreply.github.com>
2025-11-01 00:17:02 +0800

    Update CORS configuration in server.js to allow requests from an additional origin, 'https://vengeth.github.io', enhancing cross-origin resource sharing capabilities.

--- server.js
+++ server.js
@@ -61,7 +61,10 @@ app.use(helmet({
 
 // * CORS configuration
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: [
+    process.env.CORS_ORIGIN || 'http://localhost:3000',
+    'https://vengeth.github.io'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
